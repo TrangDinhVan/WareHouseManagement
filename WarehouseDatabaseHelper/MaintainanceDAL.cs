b315a8193263a5428f82ea37a6f940aed95589c9
Trang Dinh Van <trangdv@outlook.com>
2014-10-24 13:18:45 +0700

    Step

---
+++ WarehouseDatabaseHelper/MaintainanceDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using
+using WarehouseEntity;
+using System.Data;
+
+namespace WarehouseDatabaseHelper
+{
+    public class MaintainanceDAL
+    {
+        public int createMaintain(Maintainance maitain)
+        {
+            string query = string.Format("");
+            return new Connection().exeNonQuery(query);
+        }
+
+        public int deleteMaintain(Maintainance maintain)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DataTable getAllMaintain()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Maintainance getOneMaintain(int ID)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int getSomeMaintain(string query)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int updateMaintain(int ID)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
